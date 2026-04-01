b4c6763b66591b1f75cbde28882901de13973e1d
ViggaD2003 <thanhdanhvt2003@gmail.com>
2026-04-01 22:50:03 +0700

    fix redeem controller

--- src/main/java/com/summor/summorwar/controller/GiftCodeController.java
+++ src/main/java/com/summor/summorwar/controller/GiftCodeController.java
@@ -4,6 +4,7 @@ import com.summor.summorwar.models.GameAccount;
 import com.summor.summorwar.service.ExcelService;
 import com.summor.summorwar.service.GiftCodeAutomationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,12 +17,11 @@ public class GiftCodeController {
     private final ExcelService excelService;
     private final GiftCodeAutomationService automationService;
 
-    @PostMapping("/redeem")
+    @PostMapping(value = "/redeem", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String redeemGiftCode(
-            @RequestBody() MultipartFile file,
+            @RequestParam("file") MultipartFile file,
             @RequestParam("code") String giftCode
     ) {
-
         List<GameAccount> accounts = excelService.readExcel(file);
 
         automationService.redeemForAccounts(accounts, giftCode);
